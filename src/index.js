b89059d311bab16836d3c1d649ac7d91027b527c
Andreizayaz <poznavauka@mail.ru>
2020-10-09 22:45:23 +0300

    feat: solve task morse-decoder.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,53 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	//  swap key value in object	
+	var newObj = Object.keys(MORSE_TABLE).reduce((accumulator, key) => {
+		if (MORSE_TABLE.hasOwnProperty(key)) {
+			accumulator[MORSE_TABLE[key]] = key;
+		}
+		return accumulator;
+	}, {});
+
+	//  decode value in object
+	for(let key in newObj){
+		newObj[key] = newObj[key].replace(/-/gi,'11');
+		newObj[key] = newObj[key].replace(/\./gi,'10');
+		newObj[key] = newObj[key].split('');
+		
+		if(newObj[key].length<10){
+			newObj[key] = newObj[key].reverse();
+		while(newObj[key].length<10){
+		 newObj[key].push('0');
+		}
+		newObj[key] = newObj[key].reverse().join('');
+	  }else{
+	  	newObj[key] = newObj[key].join('');
+	  }
+		
+	  }
+
+	  newObj[' '] = '**********';
+
+	  //  get a result
+
+	let arr = expr.split('');
+	let resultStr = ''
+	let temp1='';
+	let temp2='';
+
+	for(let i=0; i<arr.length;i+=10){
+		for(let key in newObj){
+			temp1 = newObj[key];
+			temp2 = arr.slice(i,i+10).join('');
+		  if(newObj[key]===arr.slice(i,i+10).join('')){
+		  	resultStr += key;
+			break;
+		  } 
+		}
+	  }
+
+  return resultStr;
 }
 
 module.exports = {
